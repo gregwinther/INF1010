9b9279140f4bbecbb356e479c547960dcd30c87e
gregwinther <sebastwi@uio.no>
2017-04-02 21:44:26 +0200

    Solved practice problem 2.5

---
+++ 2017/practice/2.5/Main.java
@@ -0,0 +1,12 @@
+/* 
+   Dette Main-programmet demonstrerer hvordan en klasse 
+   automatisk arver fra klassen Object. 
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        TomKlasse tomKlasse = new TomKlasse();
+        System.out.println(tomKlasse.toString());
+        System.out.println(tomKlasse);
+    }
+}
---
+++ 2017/practice/2.5/TomKlasse.java
@@ -0,0 +1,8 @@
+// Dette er en tom klasse. Den gjoer ingenting.
+
+public class TomKlasse {
+
+    TomKlasse() {
+        
+    }
+}
